95225bef72b720569453b626321e679a14d0eb0e
curioyang <curioyhq@gmail.com>
2021-11-15 10:09:38 +0800

    fix fold mul add to dw

--- src/transforms/neutral/optimize_resnet50.cpp
+++ src/transforms/neutral/optimize_resnet50.cpp
@@ -31,6 +31,8 @@ bool fold_mul_add_to_dw_transform::on_try_match(node &node, transform_context &c
     {
         if (b_mul->binary_op() == binary_mul)
         {
+            if (try_get_direct_parent<conv2d>(*b_mul, 0) || try_get_direct_parent<conv2d>(*b_mul, 1))
+                return false;
             if (auto b_add = try_get_direct_child<binary>(*b_mul))
             {
                 if (b_add->output().connections().size() == 1 && b_add->output().connections()[0]->owner().runtime_opcode() == op_conv2d)
@@ -70,7 +72,6 @@ bool fold_mul_add_to_dw_transform::on_try_match(node &node, transform_context &c
 
                         context.outputs.emplace_back(&b_add->output());
                         context.matched_nodes.emplace_back(b_mul);
-                        context.matched_nodes.emplace_back(b_add);
                         return true;
                     }
                 }
@@ -84,45 +85,23 @@ bool fold_mul_add_to_dw_transform::on_try_match(node &node, transform_context &c
 void fold_mul_add_to_dw_transform::process(transform_context &context)
 {
     auto &output = *context.inputs[0]->connection();
-    auto &weights = *context.inputs[1]->connection();
-    auto &bias = *context.inputs[2]->connection();
     auto inputs = context.outputs[0]->connections();
-
     auto &b_mul = static_cast<binary &>(*context.matched_nodes[0]);
-    auto &b_add = static_cast<binary &>(*context.matched_nodes[1]);
 
-    auto bitc_weights = context.graph.emplace<bitcast>(weights.type(), weights.shape(), shape_t { weights.shape()[0], 1, 1, 1 });
-    auto bitc_bias = context.graph.emplace<bitcast>(bias.type(), bias.shape(), shape_t { bias.shape()[0] });
+    std::vector<float> ones(int(output.shape()[1]), 1.f);
+    std::vector<float> zeros(int(output.shape()[1]), 0.f);
 
-    // auto in_h = (int32_t)output.shape()[2];
-    // auto in_w = (int32_t)output.shape()[3];
-    auto groups = (int32_t)bitc_weights->output().shape()[0];
-    // auto f_h = (int32_t)bitc_weights->output().shape()[2];
-    // auto f_w = (int32_t)bitc_weights->output().shape()[3];
-    // auto stride_h = 1;
-    // auto stride_w = 1;
-    // auto dilation_h = 1;
-    // auto dilation_w = 1;
-    // auto pad_h = get_windowed_padding(in_h, f_h, stride_h, dilation_h, 0);
-    // auto pad_w = get_windowed_padding(in_w, f_w, stride_w, dilation_w, 0);
-    // conv2d *conv;
-    auto clamp = b_add.fused_activation();
+    auto one = context.graph.emplace<constant>(dt_float32, shape_t { output.shape()[1], 1, 1, 1 }, ones);
+    auto zero = context.graph.emplace<constant>(dt_float32, shape_t { output.shape()[1] }, zeros);
+    auto groups = (int32_t)output.shape()[1];
 
-    auto conv = context.graph.emplace<conv2d>(output.shape(), bitc_weights->output().shape(), groups,
-        padding { 0, 0 }, padding { 0, 0 }, 1, 1, 1, 1, clamp);
-    // conv->name(get_tensor(op.outputs(), 0).name()->string_view());
+    auto conv = context.graph.emplace<conv2d>(output.shape(), one->output().shape(), groups,
+        padding { 0, 0 }, padding { 0, 0 }, 1, 1, 1, 1, value_range<float>::full());
 
-    // auto conv = context.graph.emplace<conv2d>(output.shape(), bitc_weights->output().shape(), 1, padding { 0, 0 }, padding { 0, 0 }, 1, pad_h, pad_w, stride_h, stride_w, dilation_h, dilation_w, clamp);
-
-    bitc_weights->name(b_mul.name() + "/fused_dw" + "_bitc_weights");
-    bitc_weights->input().connect(weights);
-    bitc_bias->name(b_mul.name() + "/fused_dw" + "_bitc_bias");
-    bitc_bias->input().connect(bias);
     conv->name(b_mul.name() + "/fused_dw");
     conv->input().connect(output);
-    conv->weights().connect(bitc_weights->output());
-    conv->bias().connect(bitc_bias->output());
+    conv->weights().connect(one->output());
+    conv->bias().connect(zero->output());
 
-    for (auto &in : dup(inputs))
-        in->connect(conv->output());
+    context.inputs[0]->connect(conv->output());
 }
